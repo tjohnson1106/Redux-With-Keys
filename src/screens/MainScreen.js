36ae509defaee71361b09600931e37f52d82aa88
Thomas_Johnson <tjohnson1106@gmail.com>
2018-02-19 20:54:03 -0500

    added image caching

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -6,15 +6,12 @@ import styled from "styled-components/native";
 import icon from "../../assets/icons/pure-icon.png";
 import { STATUS_BAR_HEIGHT } from "../constants/index";
 
-function cacheImages(images) {
-  return images.map(images => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
+const cacheImages = images =>
+  images.map(image => {
+    if (typeof image === "string") return Image.prefetch(image);
 
-      return Expo.Asset.fromModule(image).downloadAsync();
-    }
+    return Expo.Asset.fromModule(image).downloadAsync();
   });
-}
 
 const MainView = styled.View`
   flex: 1;
@@ -40,6 +37,21 @@ class MainScreen extends Component {
     },
     headerLeft: <ImageIcon source={icon} />
   });
+
+  state = {
+    appIsReady: false
+  };
+
+  componentWillMount() {
+    this._loadAssetsAsync();
+  }
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([icon]);
+    await Promise.all([...imageAssets]);
+    this.setState({ appIsReady });
+  }
+
   render() {
     return <MainView>{/* Chord Modal */}</MainView>;
   }
